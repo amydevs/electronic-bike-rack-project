1a077a7a5a5a0ef8ba5fe8191ef4a46fdc85ad21
Amy <ctrlalternativedelete@gmail.com>
2021-02-24 11:51:58 +1100

    made sqlite conversion function

--- index.py
+++ index.py
@@ -1,5 +1,31 @@
 import sqlite3
-conn = sqlite3.connect('main.sqlite')
-c = conn.cursor()
-rows = c.execute("SELECT * FROM active;").fetchall()
-print(rows)
+
+def convertSqlite(tableName):
+    conn = sqlite3.connect('main.sqlite')
+    c = conn.cursor()
+    
+    RowsArray=[]
+
+    RowsDescription=[]
+    
+    execution=c.execute("SELECT * FROM "+tableName+";")
+
+    for row in execution.description:
+        RowsDescription.append(row[0])
+
+    for row in execution.fetchall():
+        rowDict={}
+        RowsArrayIndex=0
+        for name in RowsDescription:
+            rowDict[RowsDescription[RowsArrayIndex]] = row[RowsArrayIndex]
+            RowsArrayIndex=RowsArrayIndex+1
+        RowsArray.append(rowDict)
+    conn.close()
+    return RowsArray
+
+racks=convertSqlite('racks')
+users=convertSqlite('users')
+active=convertSqlite('active')
+
+input = input("Enter the number you would like to use: ")
+
