0e2939ae955f4ac4db2bc5a7b77c7123eb62670b
Amy <ctrlalternativedelete@gmail.com>
2021-02-26 10:43:53 +1100

    fixed up functions

--- index.py
+++ index.py
@@ -5,6 +5,6 @@ from modules.barcodemanager import BarcodeManager
 racks=SqliteManager.convertSqlite('racks')
 users=SqliteManager.convertSqlite('users')
 active=SqliteManager.convertSqlite('active')
-image = BarcodeManager.camera()
-print(BarcodeManager.decode(image))
+
+print(BarcodeManager.camera())
 
--- modules/barcodemanagerold.py
+++ modules/barcodemanagerold.py
@@ -13,7 +13,6 @@ class BarcodeManager:
         cv2.namedWindow("Test")
         img_counter = 0
         img = ""
-        returnmessage = ""
         while img_counter == 0:
             ret, frame = cam.read()
             if not ret:
@@ -21,28 +20,21 @@ class BarcodeManager:
                 break
             cv2.imshow("Test", frame)
 
-            returnmessage = BarcodeManager.decode(frame)
-            if (len(returnmessage)>0) :
-                img_counter += 1
-                
-
             k = cv2.waitKey(1)
             if k%256 == 27:
                 # ESC pressed
                 print("Escape hit, closing...")
-                returnmessage='none'
                 break
             elif k%256 == 32:
                 # SPACE pressed 
                 # img_name = "opencv_frame_{}.png".format(img_counter)
                 # cv2.imwrite(img_name, frame)
                 # print("{} written!".format(img_name))
+                img = frame
                 img_counter += 1
-                returnmessage='none'
         cam.release()
         cv2.destroyAllWindows()
-        return returnmessage
-        
+        return img
     def decode(image):
     # decodes all barcodes from an image
 
@@ -54,9 +46,9 @@ class BarcodeManager:
         # image_new = np.ones_like(image_thr) * 255
         # image_new[35:175, idx] = 0
         # image = io.imread('https://i1.wp.com/techtutorialsx.com/wp-content/uploads/2020/01/barcode.png?w=788&ssl=1')
-        mask = cv2.inRange(image,(0,0,0),(200,200,200))
-        thresholded = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
-        image = 255-thresholded # black-in-white
+        # mask = cv2.inRange(image,(0,0,0),(200,200,200))
+        # thresholded = cv2.cvtColor(mask,cv2.COLOR_GRAY2BGR)
+        # image = 255-thresholded # black-in-white
         # cv2.imshow('image_thr', image)
         # cv2.waitKey(0)
         # cv2.destroyAllWindows()
@@ -71,7 +63,6 @@ class BarcodeManager:
         #     print("Data:", obj.data)
         #     print()
         return decoded_objects
-
     def draw_barcode(decoded, image):
         # n_points = len(decoded.polygon)
         # for i in range(n_points):
